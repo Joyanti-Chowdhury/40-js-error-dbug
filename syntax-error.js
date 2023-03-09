65e9ed0a123ea41bfc7db2e9774a60ea55bf52a9
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-03-09 14:36:55 +0600

    first commit

---
+++ syntax-error.js
@@ -0,0 +1,29 @@
+function isBigger(num){
+    if(num > 5 ){
+        return true;
+
+    }
+    else return false;
+}
+  // }
+
+const result = isBigger(11)
+// console.log(result);
+
+// const sum =a, b => a + b;
+const sum =(a, b) => a + b;
+const res = sum(3,5);
+
+const musk = {
+    name :'elon',
+    // age:53
+    age:53,
+    job:'tesla'
+}
+console.log(mask.age);
+
+const numbers = [34,34,45,64,87] 
+
+// for (let i = 0)
+
+
---
+++ type-error.js
@@ -0,0 +1,10 @@
+// const num = 5;
+// num.push(45)
+
+let address = {city: 'ctg',post:4344};
+// console.log(address.home.building);
+console.log(address?.home?.building);
+
+
+const a = 'abul';
+a = 'babul'
